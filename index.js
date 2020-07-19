b7488142705b0c1933686343888cb8296c7f9bc2
Shyaboi <ian.shane.sears@gmail.com>
2020-07-18 20:47:35 -0700

    no string

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class Server extends Pop {
     var path = require('path');
     var router = express.Router();
     const app = express()
-    var portNumber = ''
+    var portNumber
     const port = portNumber
     
     app.get("/", (req, res) => res.send(content))
